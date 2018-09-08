16ada19c1d1003a7d8808ae3757cea3d75d90fee
Denys Korotin <deniskorotin4@gmail.com>
2018-09-08 20:03:00 +0300

    Fix some bugs

--- src/app/github-searcher/components/search/search.component.ts
+++ src/app/github-searcher/components/search/search.component.ts
@@ -12,15 +12,18 @@ export class SearchComponent {
 
   public searchString = '';
   public user: any;
-  public minLength: number = 3;
+  public minLength = 3;
 
   constructor(private profile: ProfileService) { }
 
   public handleChange() {
     if (this.searchString.length > this.minLength){
       this.profile.getProfile(this.searchString)
-      .pipe(debounceTime(500))
+      .pipe(debounceTime(1000))
       .subscribe(user => {
+        if (!user) {
+          return;
+        }
         return this.user = user;
       });
     }
--- src/app/github-searcher/profile.service.ts
+++ src/app/github-searcher/profile.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, debounceTime } from 'rxjs/operators';
 
-const CLIENT_ID: string = '3232f17992e4ea4ffa62';
-const CLIENT_SECRET: string = '9ce429ddfb39a59730712f8e245f645ae06c1212';
+const CLIENT_ID = '3232f17992e4ea4ffa62';
+const CLIENT_SECRET = '9ce429ddfb39a59730712f8e245f645ae06c1212';
 
 @Injectable({
   providedIn: 'root'
@@ -16,6 +16,7 @@ export class ProfileService {
   getProfile(username: string): Observable<any> {
     return this.http
       .get(`https://api.github.com/users/${username}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`)
-      .pipe(map((response: Response) => response))
+      .pipe(debounceTime(1000))
+      .pipe(map((response: Response) => response));
   }
 }
